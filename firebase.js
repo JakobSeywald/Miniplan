ee880288e75e04d523652f638d9460c8d530aec9
JakobSeywald <jakob.seywald@gmail.com>
2026-01-08 12:49:16 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,17 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore, collection, doc, setDoc, getDocs, onSnapshot, query, orderBy, updateDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-// ---------- Firebase-Konfiguration ----------
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+// 🔥 DEINE Firebase-Daten
 const firebaseConfig = {
   apiKey: "AIzaSyBxyUvXtwqI9-Jb0iUhUuSgqmbMJMAgzAg",
   authDomain: "miniplan-43d9c.firebaseapp.com",
   projectId: "miniplan-43d9c",
-  storageBucket: "miniplan-43d9c.firebasestorage.app",
+  storageBucket: "miniplan-43d9c.appspot.com",
   messagingSenderId: "284553649544",
   appId: "1:284553649544:web:a96421b904948833b22e3d"
 };
 
+// Initialisieren
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
